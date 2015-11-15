d969f392eef395ecd8f9913458cfe47fe5ecca17
Ariel Jakobovits <arieljake@gmail.com>
2015-11-15 21:36:10 +0200

    update bower

--- dist/angular-mapbox.js
+++ dist/angular-mapbox.js
@@ -173,14 +173,6 @@
             {
                 var featureLayer;
 
-                var createHtmlIcon = function(e)
-                {
-                    var marker = e.layer;
-                    var feature = marker.feature;
-
-                    marker.setIcon(L.divIcon(feature.properties.icon));
-                };
-
                 controller.getMap().then(function(map)
                 {
                     if (attrs.data)
@@ -192,21 +184,14 @@
                     {
                         featureLayer = L.mapbox.featureLayer(attrs.url);
                     }
-                    else
-                    {
-                        featureLayer = L.mapbox.featureLayer(null);
-                    }
 
                     if (featureLayer)
                     {
                         featureLayer.addTo(map);
                         controller.addFeatureLayer(featureLayer);
 
-                        featureLayer.on('layeradd', createHtmlIcon);
-
                         element.bind('$destroy', function()
                         {
-                            featureLayer.off('layeradd', createHtmlIcon);
                             map.removeLayer(featureLayer);
                             controller.removeFeatureLayer(featureLayer);
                         });
@@ -217,10 +202,12 @@
     });
 })();
 
-(function() {
+(function()
+{
     'use strict';
 
-    angular.module('angular-mapbox').directive('htmlMarker', function($compile, $timeout, mapboxService) {
+    angular.module('angular-mapbox').directive('htmlMarker', function($compile, $timeout, mapboxService)
+    {
         var _colors = {
             navy: '#001f3f',
             blue: '#0074d9',
@@ -249,96 +236,135 @@
             link: link
         };
 
-        function link(scope, element, attrs, controller, transclude) {
+        function link(scope, element, attrs, controller, transclude)
+        {
             var _marker, _opts, _style;
 
             _opts = {
                 draggable: attrs.draggable !== undefined,
-                className:  attrs.className,
+                className: attrs.className,
                 html: attrs.html,
                 width: attrs.width,
-                height:  attrs.height
+                height: attrs.height
             };
 
             _style = setStyleOptions(attrs);
 
-            controller.getMap().then(function(map) {
-                transclude(scope, function(transcludedContent) {
+            controller.getMap().then(function(map)
+            {
+                transclude(scope, function(transcludedContent)
+                {
                     var popupContentElement;
 
-                    if(transcludedContent !== null && transcludedContent.length > 0) {
+                    if (transcludedContent !== null && transcludedContent.length > 0)
+                    {
                         popupContentElement = document.createElement('span');
-                        for(var i = 0; i < transcludedContent.length; i++) {
+                        
+                        for (var i = 0; i < transcludedContent.length; i++)
+                        {
                             popupContentElement.appendChild(transcludedContent[i]);
                         }
                     }
 
-                    if(attrs.currentLocation !== undefined) {
-                        _style = setStyleOptions(_style, { 'marker-color': '#000', 'marker-symbol': 'star' });
+                    if (attrs.currentLocation !== undefined)
+                    {
+                        _style = setStyleOptions(_style,
+                        {
+                            'marker-color': '#000',
+                            'marker-symbol': 'star'
+                        });
                         _opts.excludeFromClustering = true;
 
-                        map.on('locationfound', function(e) {
+                        map.on('locationfound', function(e)
+                        {
                             _marker = addMarker(scope, map, [e.latlng.lat, e.latlng.lng], popupContentElement, _opts, _style);
                         });
 
                         map.locate();
-                    } else {
+                    }
+                    else
+                    {
                         _marker = addMarker(scope, map, [attrs.lat, attrs.lng], popupContentElement, _opts, _style);
                     }
                 });
 
-                element.bind('$destroy', function() {
-                    if(mapboxService.getOptionsForMap(map).clusterMarkers) {
+                element.bind('$destroy', function()
+                {
+                    if (mapboxService.getOptionsForMap(map).clusterMarkers)
+                    {
                         scope.clusterGroup.removeLayer(_marker);
-                    } else {
+                    }
+                    else
+                    {
                         mapboxService.removeMarker(map, _marker);
                     }
                 });
             });
         }
 
-        function setStyleOptions(attrs, defaultOpts) {
-            var opts = defaultOpts || {};
-            if(attrs.size) {
+        function setStyleOptions(attrs, defaultOpts)
+        {
+            var opts = defaultOpts ||
+            {};
+
+            if (attrs.size)
+            {
                 opts['marker-size'] = attrs.size;
             }
-            if(attrs.color) {
-                if(attrs.color[0] === '#') {
+
+            if (attrs.color)
+            {
+                if (attrs.color[0] === '#')
+                {
                     opts['marker-color'] = attrs.color;
-                } else {
+                }
+                else
+                {
                     opts['marker-color'] = _colors[attrs.color] || attrs.color;
                 }
             }
-            if(attrs.icon) {
+
+            if (attrs.icon)
+            {
                 opts['marker-symbol'] = attrs.icon;
             }
+
             return opts;
         }
 
-        function addMarker(scope, map, latlng, popupContent, opts) {
-            opts = opts || {};
+        function addMarker(scope, map, latlng, popupContent, opts)
+        {
+            opts = opts ||
+            {};
 
-            var marker = L.marker(latlng, {
-                icon: L.divIcon({
+            var marker = L.marker(latlng,
+            {
+                icon: L.divIcon(
+                {
                     className: opts.className,
                     html: opts.html,
                     iconSize: opts.width && opts.height ? [opts.width, opts.height] : null
                 })
             });
 
-            if(popupContent) {
+            if (popupContent)
+            {
                 marker.bindPopup(popupContent);
             }
 
-            if(mapboxService.getOptionsForMap(map).clusterMarkers && opts.excludeFromClustering !== true) {
+            if (mapboxService.getOptionsForMap(map).clusterMarkers && opts.excludeFromClustering !== true)
+            {
                 scope.clusterGroup.addLayer(marker);
-            } else {
+            }
+            else
+            {
                 marker.addTo(map);
             }
 
             // this needs to come after being added to map because the L.mapbox.marker.style() factory
             // does not let us pass other opts (eg, draggable) in
-            if(opts.draggable) {
+            if (opts.draggable)
+            {
                 marker.dragging.enable();
             }
 
