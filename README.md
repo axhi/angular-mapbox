8dffcb11db402862e8ee3ee0ead5b58476e39aff
Joey Sabani <joey.sabani@gmail.com>
2015-12-23 11:55:32 -0600

    Updating

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # angular-mapbox
 
-Install with `bower install angular-mapbox`.
+Install with `bower install angular-mapbox-earshot`.
 
 Please see the [documentation](http://licyeus.github.io/angular-mapbox) for usage.
+
+This is a forked version of the above documentation set.
--- gulpfile.js
+++ gulpfile.js
@@ -11,25 +11,6 @@
         jshint = require('gulp-jshint'),
         stylish = require('jshint-stylish');
 
-    gulp.task('scripts', function()
-    {
-        return gulp.src(['src/angular-mapbox-v2.module.js', 'src/**/*.js'])
-            .pipe(concat('angular-mapbox-v2.js'))
-            .pipe(gulp.dest('dist'));
-    });
-
-    gulp.task('build', function()
-    {
-        return gulp.src(['src/angular-mapbox-v2.module.js', 'src/**/*.js'])
-            .pipe(concat('angular-mapbox-v2.min.js'))
-            .pipe(ngmin())
-            .pipe(uglify(
-            {
-                mangle: false
-            }))
-            .pipe(gulp.dest('dist'));
-    });
-
     gulp.task('watch', ['scripts'], function()
     {
         var server = livereload();
